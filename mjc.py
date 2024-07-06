19a7568aa0000c3d12c3e7de89bb07b23553637c
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2024-07-06 21:44:04 +0300

    Simplify tracking code for MuJoCo

--- mjc.py
+++ mjc.py
@@ -4,44 +4,27 @@ import threading
 from dm_control import mujoco
 from dm_control.utils import inverse_kinematics as ik
 from flask import Flask, request, jsonify
-from scipy.spatial.transform import Rotation as R
 import numpy as np
 import rerun as rr
 
-rr.init("franka_sim", spawn=False)
-rr.save("mujoco_sim.rr")
+rr.init("mujoco_sim", spawn=False)
 # rr.connect()
+rr.save("mujoco.rrd")
+
 
 app = Flask(__name__, static_url_path='', static_folder='quest_tracking/static')
-tracker_position = {"transform": None, "but": False}
+tracker_position = {"pos": None, "quat": None, "but": (False, False)}
 tracker_position_lock = threading.Lock()
 
-class Transform3D:
-    def __init__(self, position, orientation):
-        self.position = position  # np.array([x, y, z])
-        self.orientation = R.from_quat(orientation)  # np.quaternion([w, x, y, z])
+def q_inv(q):
+    result = np.zeros(4)
+    mujoco.mju_negQuat(result, q)
+    return result
 
-    def __mul__(self, other):
-        # new_position = self.position + self.orientation.apply(other.position)
-        new_position = self.position + other.position
-        new_orientation = self.orientation * other.orientation
-        return Transform3D(new_position, new_orientation.as_quat())
-
-    def inverse(self):
-        inv_orientation = self.orientation.inv()
-        inv_position = -self.position
-        return Transform3D(inv_position, inv_orientation.as_quat())
-
-    def __str__(self):
-        pos_str = ", ".join([f"{v:.3f}" for v in self.position])
-        ori_str = ", ".join([f"{v:.3f}" for v in self.orientation.as_quat()])
-        return f"Tr3D(position={pos_str}, orientation={ori_str})"
-
-    @property
-    def rerun(self):
-        q = self.orientation.as_quat()
-        return rr.Transform3D(translation=self.position,
-                              rotation=rr.Quaternion(xyzw=[q[1], q[2], q[3], q[0]]))
+def q_mul(q1, q2):
+    result = np.zeros(4)
+    mujoco.mju_mulQuat(result, q1, q2)
+    return result
 
 latest_request_queue = queue.Queue(maxsize=1)
 
@@ -67,11 +50,19 @@ def process_latest_request():
 
         if last_ts is None or data['timestamp'] > last_ts:
             last_ts = data['timestamp']
-            pos = Transform3D(position=np.array(data['position']),
-                            orientation=np.array(data['orientation']))
-            but = data['buttons'][4] if len(data['buttons']) > 4 else False
+            pos = np.array(data['position'])
+            quat = np.array(data['orientation'])
+            but = (data['buttons'][4], data['buttons'][5]) if len(data['buttons']) > 5 else (False, False)
+
+            pos = np.array([-pos[2], -pos[0], pos[1]])
+            # Compute updated rotation quaternion given coordinate system transformation above
+            # Transform the orientation to the new coordinate system, where Z points up, X points forward, and Y points left
+            transformed_quat = np.array([quat[3], quat[2], -quat[0], quat[1]])
+            quat = transformed_quat
+
             with tracker_position_lock:
-                tracker_position['transform'] = pos
+                tracker_position['pos'] = pos
+                tracker_position['quat'] = quat
                 tracker_position['but'] = but
 
         latest_request_queue.task_done()
@@ -95,66 +86,55 @@ def main():
     site_id = model.site('end_effector').id
     mocap_ee_id, mocap_tracker_id = 0, 1
 
-    tracking_shift = None
+    def get_real_ee_position():
+        return data.site_xpos[site_id], xmat_to_quat(data.site_xmat[site_id])
+        # return data.mocap_pos[mocap_ee_id], data.mocap_quat[mocap_ee_id]
+
+    is_tracking = False
+    tr_diff = None
     tracker_origin = None
     franka_origin = None
     target = None
 
-    def get_real_ee_position():
-        return Transform3D(data.mocap_pos[0], data.mocap_quat[0])
-        # return Transform3D(data.site_xpos[site_id], xmat_to_quat(data.site_xmat[site_id]))
-
-    with mujoco.viewer.launch_passive(model, data) as viewer:
+    with mujoco.viewer.launch_passive(model, data, show_right_ui=False) as viewer:
+        rr.set_time_seconds("time", data.time)
         while viewer.is_running():
-            rr.set_time_seconds("time", data.time)
-
             with tracker_position_lock:
-                if tracker_position['but']:
-                    tracker_origin = tracker_position['transform']
-                    franka_origin = get_real_ee_position()
-                    # tracking_shift = franka_origin * tracker_origin.inverse()
-                    tracking_shift = tracker_origin.inverse() * franka_origin
-                elif tracking_shift is not None:
-                    target = tracker_position['transform'] * tracking_shift
+                pos, quat, but = tracker_position['pos'], tracker_position['quat'], tracker_position['but']
+                quat = np.array([1.0, 0., 0., 0.])
 
-                    # tracking_move = tracker_origin.inverse() * tracker_position['transform']
-                    # franka_move = franka_origin.inverse() * Transform3D(data.qpos[:3], data.qpos[3:7])
-                    # log_transform("move/tracking", tracking_move)
-                    # log_transform("move/franka", franka_move)
+            if but[0]:
+                franka_origin = get_real_ee_position()
+                tr_diff = franka_origin[0] - pos, q_mul(q_inv(quat), franka_origin[1])
+                is_tracking = True
+            elif but[1]:
+                is_tracking = False
+            elif is_tracking:
+                target = pos + tr_diff[0], q_mul(quat, tr_diff[1])
 
-            # real_pos = get_real_ee_position()
-            # rr.log("position/current", real_pos.rerun)
-            # for i, v in zip('xyz', real_pos.position):
-            #     rr.log(f"translation/current/{i}", rr.Scalar(v))
-            # for i, v in zip('wxyz', real_pos.orientation.as_quat()):
-            #     rr.log(f"rotation/current/{i}", rr.Scalar(v))
+            data.mocap_pos[mocap_tracker_id] = pos
+            data.mocap_quat[mocap_tracker_id] = quat
 
-            if target is not None:
-                data.mocap_pos[mocap_ee_id] = target.position
-                data.mocap_quat[mocap_ee_id] = target.orientation.as_quat()
+            if target is not None and is_tracking:
+                data.mocap_pos[mocap_ee_id] = target[0]
+                data.mocap_quat[mocap_ee_id] = target[1]
 
-                data.mocap_pos[mocap_tracker_id] = tracker_position['transform'].position
-                data.mocap_quat[mocap_tracker_id] = tracker_position['transform'].orientation.as_quat()
+                result = ik.qpos_from_site_pose(
+                    physics=physics,
+                    site_name='end_effector',
+                    target_pos=target[0],
+                    # target_quat=target[1],
+                    joint_names=joints,
+                )
 
-                # result = ik.qpos_from_site_pose(
-                #     physics=physics,
-                #     site_name='end_effector',
-                #     target_pos=target.position,
-                #     target_quat=target.orientation.as_quat(),
-                #     joint_names=joints,
-                # )
+                if result.success:
+                    for i in range(7):
+                        rr.log(f'ik/{i}', rr.Scalar(result.qpos[i]))
+                        rr.log(f'ctrl/{i}', rr.Scalar(data.ctrl[i]))
 
-                # rr.log("position/target", target.rerun)
-                # for i, v in zip('xyz', target.position):
-                #     rr.log(f"translation/target/{i}", rr.Scalar(v))
-                # for i, v in zip('wxyz', target.orientation.as_quat()):
-                #     rr.log(f"rotation/target/{i}", rr.Scalar(v))
-
-                # if result.success:
-                #     data.ctrl[:7] = result.qpos[:7]
-                # else:
-                #     log += f"IK failed: target: {target}"
-                #     print(log)
+                    data.ctrl[:7] = result.qpos[:7]
+                else:
+                    print(f"IK failed: target: {target}")
 
             mujoco.mj_step(model, data)
             viewer.sync()
