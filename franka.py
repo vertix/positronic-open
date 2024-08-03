341d79ec95c45701733f2dd2ea8b0d3621c2eb21
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2024-08-04 00:07:24 +0300

    Port franka script to web_server architecture

--- franka.py
+++ franka.py
@@ -1,24 +1,35 @@
+from contextlib import asynccontextmanager
 import queue
 import logging
-import threading
 import time
 import sys
+import threading
 
-from flask import Flask, request, jsonify
+from fastapi import BackgroundTasks, Depends, FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from starlette.responses import FileResponse
+import uvicorn
 import franky
 from franky import Affine, CartesianMotion, ReferenceType, RealtimeConfig
 import numpy as np
 import rerun as rr
 from scipy.spatial.transform import Rotation as R
 
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler('franka.log'),
+        logging.StreamHandler(sys.stderr)
+    ]
+)
+logger = logging.getLogger(__name__)
 
 rr.init("franka", spawn=False)
 rr.connect('192.168.10.3:9876')
 rr.save("franka.rrd")
-
-app = Flask(__name__, static_url_path='', static_folder='quest_tracking/static')
-tracker_position = {"pos": None, "quat": None, "but": (False, False)}
-tracker_position_lock = threading.Lock()
 
 def q_inv(q):
     r = R.from_quat([q[1], q[2], q[3], q[0]])
@@ -31,92 +42,9 @@ def q_mul(q1, q2):
     res = (r1 * r2).as_quat()
     return np.array([res[3], res[0], res[1], res[2]])
 
-latest_request_queue = queue.Queue(maxsize=1)
-
-@app.route('/track', methods=['POST'])
-def track():
-    data = request.json
-    try:
-        # Try to put the new request data into the queue
-        latest_request_queue.put_nowait(data)
-    except queue.Full:
-        # If the queue is full, replace the existing item
-        latest_request_queue.get_nowait()
-        latest_request_queue.put_nowait(data)
-    return jsonify(success=True)
-
-def process_latest_request():
-    last_ts = None
-    while True:
-        try:
-            data = latest_request_queue.get(timeout=1)
-        except queue.Empty:
-            continue
-
-        if last_ts is None or data['timestamp'] > last_ts:
-            last_ts = data['timestamp']
-            pos = np.array(data['position'])
-            quat = np.array(data['orientation'])
-            but = (data['buttons'][4], data['buttons'][5]) if len(data['buttons']) > 5 else (False, False)
-
-            pos = np.array([-pos[2], -pos[0], pos[1]])
-            quat = np.array([quat[0], -quat[3], -quat[1], quat[2]])
-            # Rotate quat 90 degrees around Y axis
-            rotation_y_90 = np.array([np.cos(np.pi/4), 0, np.sin(np.pi/4), 0])  # 90 degrees rotation around Y
-            quat = q_mul(rotation_y_90, quat)
-
-            with tracker_position_lock:
-                tracker_position['pos'] = pos
-                tracker_position['quat'] = quat
-                tracker_position['but'] = but
-
-        latest_request_queue.task_done()
-
-# The abstract class for a Robot, that may have different implementations
-# for different simulators and different physical robots.
-class Robot:
-    def __init__(self):
-        pass
-
-    @property
-    def forward_kinematics(self) -> tuple[np.ndarray, np.ndarray]:
-        """
-        Returns the end effector position and orientation.
-
-        Returns:
-            tuple: A tuple containing two numpy arrays:
-                - position (np.ndarray): 3D position vector
-                - orientation (np.ndarray): 4D quaternion
-        """
-        pass
-
-    def inverse_kinematics(self, target_position: np.ndarray, target_orientation: np.ndarray) -> bool:
-        """
-        Runs the inverse kinematics solver for the robot, trying to achieve the given target position and orientation.
-
-        Args:
-            target_position (np.ndarray): 3D position vector
-            target_orientation (np.ndarray): 4D quaternion
-
-        Returns:
-            bool: True if the inverse kinematics solution was found and applied, False otherwise.
-        """
-        pass
-
-    @property
-    def joint_positions(self) -> np.ndarray:
-        """
-        Returns the current joint positions of the robot.
-
-        Returns:
-            np.ndarray: joint position vector
-        """
-        pass
-
-
-class FrankaRobot(Robot):
+class FrankaRobot:
     def __init__(self, ip: str, relative_dynamics_factor: float = 0.02):
-        self.robot = franky.Robot(ip, realtime_config=RealtimeConfig.Ignore)
+        self.robot = franky.Robot(ip) #, realtime_config=RealtimeConfig.Ignore)
         self.robot.relative_dynamics_factor = relative_dynamics_factor
         self.robot.recover_from_errors()
         motion = franky.JointWaypointMotion([
@@ -130,7 +58,11 @@ class FrankaRobot(Robot):
 
     def inverse_kinematics(self, target_position: np.ndarray, target_orientation: np.ndarray) -> bool:
         pos = Affine(translation=target_position, quaternion=target_orientation)
-        self.robot.move(CartesianMotion(pos, ReferenceType.Absolute), asynchronous=True)
+        try:
+            self.robot.move(CartesianMotion(pos, ReferenceType.Absolute), asynchronous=True)
+        except franky.ControlException as e:
+            logger.warn(f"IK failed for {target_position} {target_orientation}: {e}")
+            return False
         return True
 
     @property
@@ -138,81 +70,114 @@ class FrankaRobot(Robot):
         return self.robot.current_joint_state.position
 
     def stop(self):
-        motion = franky.CartesianPoseStopMotion()
-        self.robot.move(motion)
+        self.robot.stop()
+        # motion = franky.CartesianPoseStopMotion()
+        # self.robot.move(motion)
 
-def main():
-    is_tracking = False
-    tr_diff = None
-    franka_origin = None
-    target = None
+class State:
+    def __init__(self):
+        self.is_tracking = False
+        self.tr_diff = None
+        self.franka_origin = None
+        self.target = None
+        self.robot = None
+        self.start_time = None
+        self.last_ts = None
+        self.latest_request_queue = queue.Queue(maxsize=1)
+        self.robot_update_lock = threading.Lock()
 
-    robot = FrankaRobot("172.168.0.2", 0.6)
+state = State()
+def get_state():
+    return state
 
-    start = time.time()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    state.robot = FrankaRobot("172.168.0.2", 0.3)
+    state.start_time = time.time()
+    yield
 
-    while True:
-        rr.set_time_seconds("time", time.time() - start)
+app = FastAPI(lifespan=lifespan)
 
-        with tracker_position_lock:
-            pos, quat, but = tracker_position['pos'], tracker_position['quat'], tracker_position['but']
+@app.get("/")
+async def root():
+    return FileResponse("quest_tracking/static/index.html")
 
-        if but[0]:
-            franka_origin = robot.forward_kinematics
-            tr_diff = franka_origin[0] - pos, q_mul(q_inv(quat), franka_origin[1])
-            if not is_tracking:
-                print(f'Start tracking {tr_diff}')
-            is_tracking = True
-        elif but[1]:
-            if is_tracking:
-                print('Stop tracking')
+@app.get("/webxr-button.js")
+async def webxr_button():
+    return FileResponse("quest_tracking/static/webxr-button.js")
 
-            robot.stop()
-            target = None
-            is_tracking = False
-        elif is_tracking:
-            target = pos + tr_diff[0], q_mul(quat, tr_diff[1])  # quat #
-            for i in range(3):
-                rr.log(f"target/position/{i}", rr.Scalar(target[0][i]))
-            for i in range(4):
-                rr.log(f"target/orientation/{i}", rr.Scalar(target[1][i]))
 
-        if target is not None and is_tracking:
-            # if not robot.inverse_kinematics(target[0], target[1]):
-            if not robot.inverse_kinematics(target[0], robot.forward_kinematics[1]):
-                print(f"IK failed for {target}")
+def robot_update(state: State, pos: np.ndarray, quat: np.ndarray, but: tuple[bool, bool]):
+    if but[0]:
+        state.franka_origin = state.robot.forward_kinematics
+        state.tr_diff = state.franka_origin[0] - pos, q_mul(q_inv(quat), state.franka_origin[1])
+        if not state.is_tracking:
+            logger.info(f'Start tracking {state.tr_diff}')
+        state.is_tracking = True
+    elif but[1]:
+        if state.is_tracking:
+            logger.info('Stop tracking')
 
-        t, q = robot.forward_kinematics
+        state.robot.stop()
+        state.target = None
+        state.is_tracking = False
+    elif state.is_tracking:
+        state.target = pos + state.tr_diff[0], q_mul(quat, state.tr_diff[1])  # quat #
         for i in range(3):
-            rr.log(f"franka/position/{i}", rr.Scalar(t[i]))
+            rr.log(f"target/position/{i}", rr.Scalar(state.target[0][i]))
         for i in range(4):
-            rr.log(f"franka/orientation/{i}", rr.Scalar(q[i]))
+            rr.log(f"target/orientation/{i}", rr.Scalar(state.target[1][i]))
 
-        for i, v in enumerate(robot.joint_positions):
-            rr.log(f'joints/{i}', rr.Scalar(v))
+    if state.target is not None and state.is_tracking:
+        state.robot.inverse_kinematics(state.target[0], state.robot.forward_kinematics[1])
 
+    t, q = state.robot.forward_kinematics
+    for i in range(3):
+        rr.log(f"franka/position/{i}", rr.Scalar(t[i]))
+    for i in range(4):
+        rr.log(f"franka/orientation/{i}", rr.Scalar(q[i]))
+
+    for i, v in enumerate(state.robot.joint_positions):
+        rr.log(f'joints/{i}', rr.Scalar(v))
+
+def process_latest_request(state: State):
+    try:
+        data = state.latest_request_queue.get(timeout=1)
+    except queue.Empty:
+        return
+
+    if state.last_ts is None or data['timestamp'] > state.last_ts:
+        state.last_ts = data['timestamp']
+        with state.robot_update_lock:
+            pos = np.array(data['position'])
+            quat = np.array(data['orientation'])
+            but = (data['buttons'][4], data['buttons'][5]) if len(data['buttons']) > 5 else (False, False)
+
+            pos = np.array([-pos[2], -pos[0], pos[1]])
+            quat = np.array([quat[0], -quat[3], -quat[1], quat[2]])
+            # Rotate quat 90 degrees around Y axis
+            rotation_y_90 = np.array([np.cos(np.pi/4), 0, np.sin(np.pi/4), 0])
+            quat = q_mul(rotation_y_90, quat)
+            robot_update(state, pos, quat, but)
+
+        state.latest_request_queue.task_done()
+
+@app.post('/track')
+async def track(request: Request, background_tasks: BackgroundTasks, state: State = Depends(get_state)):
+    data = await request.json()
+    try:
+        # Try to put the new request data into the queue
+        state.latest_request_queue.put_nowait(data)
+    except queue.Full:
+        # If the queue is full, replace the existing item
+        state.latest_request_queue.get_nowait()
+        state.latest_request_queue.put_nowait(data)
+
+    background_tasks.add_task(process_latest_request, state)
+    return JSONResponse(content={"success": True})
 
 if __name__ == "__main__":
-    def run_server():
-        log = logging.getLogger('werkzeug')
-        # log.setLevel(logging.ERROR)  # Set the logging level to ERROR to suppress INFO logs
-        log.setLevel(logging.CRITICAL)
-        app.logger.setLevel(logging.CRITICAL)
-
-        app.run(
-            host='0.0.0.0',
-            port=5005,
-            ssl_context=('cert.pem', 'key.pem'),
-            # debug=True
-        )
-
-    flask_thread = threading.Thread(target=run_server)
-    flask_thread.start()
-    worker_thread = threading.Thread(target=process_latest_request)
-    worker_thread.start()
-    main()
-
-    # flask_thread.join()
-    # worker_thread.join()
-
-print('Finished')
+    try:
+        uvicorn.run(app, host="0.0.0.0", port=5005, ssl_keyfile="key.pem", ssl_certfile="cert.pem")
+    except KeyboardInterrupt:
+        logger.info("Program interrupted by user")
