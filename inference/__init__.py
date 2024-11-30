91d271e5e01c31e708e018f3af9d29cd703a38bc
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2024-11-30 11:22:39 +0200

    Extract action handling into seprate module

--- inference/__init__.py
+++ inference/__init__.py
@@ -1,2 +1,3 @@
-from .inference import Inference, ActionDecoder
+from .inference import Inference
+from .action import ActionDecoder
 from .state import StateEncoder, ImageEncodingConfig, StateEncodingConfig
