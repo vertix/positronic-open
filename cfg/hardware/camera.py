86c1213a6e7e84badc0581ed488ebd368de7b4c6
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2025-02-23 16:54:19 +0200

    Camera and roboarm configurations

--- cfg/hardware/camera.py
+++ cfg/hardware/camera.py
@@ -1,47 +1,89 @@
-from typing import Dict, Union
+from typing import Dict, Optional, Tuple
 from drivers.camera.merge import merge_on_pulse
 
-from hydra_zen import builds, store, zen
+from hydra_zen import builds, store
 
 import ironic as ir
+
+def _add_image_mapping(mapping: Optional[Dict[str, str]], camera: ir.ControlSystem):
+    if mapping is None:
+        return camera
+
+    def map_images(frame):
+        return {new_k: frame[k] for k, new_k in mapping.items()}
+
+    map_system = ir.utils.MapControlSystem(map_images)
+    map_system.bind(input=camera.outs.frame)
+    return ir.compose(camera, map_system, outputs={'frame': map_system.outs.output})
+
 
 def _linux_camera(device_path: str,
                  width: int = 640,
                  height: int = 480,
                  fps: int = 30,
-                 pixel_format: str = "MJPG"):
+                 pixel_format: str = "MJPG",
+                 image_mapping: Optional[Dict[str, str]] = None):
     from drivers.camera.linuxpy_video import LinuxPyCamera
-    return LinuxPyCamera(device_path, width, height, fps, pixel_format)
+    camera = LinuxPyCamera(device_path, width, height, fps, pixel_format)
+    return _add_image_mapping(image_mapping, camera)
 
-def _luxonis_camera(*args, **kwargs):
+
+def _luxonis_camera(fps: int = 60,
+                    image_mapping: Optional[Dict[str, str]] = None):
     from drivers.camera.luxonis import LuxonisCamera
-    return LuxonisCamera(*args, **kwargs)
+    return _add_image_mapping(image_mapping, LuxonisCamera(fps))
+
+
+def _realsense_camera(resolution: Tuple[int, int] = (640, 480),
+                      fps: int = 30,
+                      enable_color: bool = True,
+                      enable_depth: bool = True,
+                      enable_infrared: bool = True,
+                      image_mapping: Optional[Dict[str, str]] = None):
+    from drivers.camera.realsense import RealsenseCamera
+    return _add_image_mapping(
+        image_mapping,
+        RealsenseCamera(resolution, fps, enable_color, enable_depth, enable_infrared))
+
+
+def _stereolabs_camera(fps: int,
+                       view: str = "LEFT",
+                       resolution: str = "AUTO",
+                       depth_mode: str = "NONE",
+                       max_depth: float = 10,
+                       coordinate_units: str = "METER",
+                       depth_mask: bool = False,
+                       image_mapping: Optional[Dict[str, str]] = None):
+    from drivers.camera.sl import SLCamera
+    import pyzed.sl as sl
+
+    view = getattr(sl.VIEW, view)
+    resolution = getattr(sl.RESOLUTION, resolution)
+    depth_mode = getattr(sl.DEPTH_MODE, depth_mode)
+    coordinate_units = getattr(sl.UNIT, coordinate_units)
+    camera = SLCamera(fps, view, resolution, depth_mode, coordinate_units, max_depth, depth_mask)
+    return _add_image_mapping(image_mapping, camera)
 
 
 linux_camera = builds(_linux_camera, populate_full_signature=True)
 luxonis_camera = builds(_luxonis_camera, populate_full_signature=True)
-
-
+realsense_camera = builds(_realsense_camera, populate_full_signature=True)
+stereolabs_camera = builds(_stereolabs_camera, populate_full_signature=True)
 merge_camera = builds(merge_on_pulse, populate_full_signature=True)
 
 
-cam_store = store(group="camera")
-cam_store(
-    linux_camera(device_path="/dev/video0", width=640, height=480, fps=30),
-    name='video0'
-)
+arducam_video0 = linux_camera(device_path="/dev/video0")
 
-cam_store(
-    linux_camera(device_path="/dev/video2", width=640, height=480, fps=30),
-    name='video2'
-)
+cam_store = store(group="hardware/cameras")
+cam_store(arducam_video0, name='arducam')
+cam_store(luxonis_camera(fps=60), name='luxonis')
+cam_store(realsense_camera(resolution=(640, 480), fps=30, enable_color=True, enable_depth=False, enable_infrared=False),
+          name='realsense')
+cam_store(stereolabs_camera(fps=30, view='SIDE_BY_SIDE', resolution='VGA', depth_mode='NONE', depth_mask=False),
+          name='sl_vga')
 
-# cam_store(luxonis_camera(fps=60), name='luxonis')
+arducam_video2 = linux_camera(device_path="/dev/video2")  # Yes, it is on video2
 cam_store(
-    merge_camera(
-        cameras={'video0': store.get_entry('camera', 'video0')['node'],
-                 'video2': store.get_entry('camera', 'video2')['node']},
-        fps=30,
-    ),
-    name='merged'
+    merge_camera(cameras={'first': arducam_video0, 'second': arducam_video2}, fps=30),
+    name='umi_merged'
 )
---
+++ cfg/hardware/roboarms.py
@@ -0,0 +1,43 @@
+from typing import List, Optional
+from hydra_zen import builds, store
+
+import ironic as ir
+
+
+def _franka(ip: str,
+            relative_dynamics_factor: float = 0.2,
+            gripper_speed: float = 0.02,
+            realtime_config: str = "Ignore",
+            collision_behavior: Optional[List[List[float]]] = None,
+            home_joints_config: Optional[List[float]] = None,
+            cartesian_mode: str = "LIBFRANKA"):
+    from drivers.roboarm.franka import Franka, CartesianMode
+    from franky import RealtimeConfig
+
+    realtime_config = getattr(RealtimeConfig, realtime_config)
+    cartesian_mode = getattr(CartesianMode, cartesian_mode)
+    return Franka(ip, relative_dynamics_factor, gripper_speed, realtime_config, collision_behavior, home_joints_config, cartesian_mode)
+
+
+franka = builds(_franka, populate_full_signature=True)
+
+
+roboarm_store = store(group="hardware/roboarms")
+
+roboarm_store(franka(ip="172.168.0.2",
+                      relative_dynamics_factor=0.2,
+                      gripper_force=0.4,
+                      home_joints_config=[0.0, -0.31, 0.0, -1.53, 0.0, 1.522, 0.785],
+                      collision_behavior=[
+                          [100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0],
+                          [100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0],
+                          [100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0],
+                          [100.0, 100.0, 100.0, 100.0, 100.0, 100.0, 100.0],
+                          [100.0, 100.0, 100.0, 100.0, 100.0, 100.0],
+                          [100.0, 100.0, 100.0, 100.0, 100.0, 100.0],
+                          [100.0, 100.0, 100.0, 100.0, 100.0, 100.0],
+                          [100.0, 100.0, 100.0, 100.0, 100.0, 100.0]
+                      ],
+                      cartesian_mode="POSITRONIC"),
+    name="franka"
+)
