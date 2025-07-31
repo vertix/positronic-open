d47af540e24ea2367ccfebdee6b867d6dcc04bee
Andrei Luzan <3865242+JIy3AHKO@users.noreply.github.com>
2025-07-31 16:34:35 +0300

    remove configuronic since it moved to separate package (#90)

--- configuronic/tests/support_package/cfg.py
+++
@@ -1,8 +0,0 @@
-import configuronic as cfn
-from configuronic.tests.support_package.subpkg.a import A
-from configuronic.tests.support_package.b import B
-
-a_cfg_value1 = cfn.Config(A, value=1)
-a_cfg_value2 = cfn.Config(A, value=2)
-b_cfg_value1 = cfn.Config(B, value=1)
-b_cfg_value2 = cfn.Config(B, value=2)
