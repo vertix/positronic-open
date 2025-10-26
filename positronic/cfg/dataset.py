ed1b9fef943a2675aab95780b603dd9e0b424b09
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2025-10-26 15:06:36 +0200

    Add task parameter to `transformed` config for optional task label

--- positronic/cfg/dataset.py
+++ positronic/cfg/dataset.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import configuronic as cfn
 
 from positronic.cfg.policy import action, observation
+from positronic.dataset.transforms.episode import KeyFuncEpisodeTransform
 
 
 @cfn.config()
@@ -12,9 +13,14 @@ def local(path: str):
     return load_all_datasets(Path(path))
 
 
-@cfn.config(observation=observation.eepose_mujoco, action=action.absolute_position, pass_through=False)
-def transformed(path, observation, action, pass_through):
+@cfn.config(observation=observation.eepose_mujoco, action=action.absolute_position, task=None, pass_through=False)
+def transformed(path, observation, action, task, pass_through):
+    """Load datasets with observation/action transforms and optional task label."""
     from positronic.dataset.local_dataset import load_all_datasets
     from positronic.dataset.transforms import TransformedDataset
 
-    return TransformedDataset(load_all_datasets(Path(path)), observation, action, pass_through=pass_through)
+    extra = []
+    if task is not None:
+        extra.append(KeyFuncEpisodeTransform(task=lambda _ep: task))
+
+    return TransformedDataset(load_all_datasets(Path(path)), observation, action, *extra, pass_through=pass_through)
