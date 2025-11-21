0910606815359036207dde9c758d5a71a5c590b3
Sergey Arkhangelskiy <mc.vertix@gmail.com>
2025-11-21 17:52:10 +0200

    Move KeyboardControl to a separate file

---
+++ positronic/gui/keyboard.py
@@ -0,0 +1,32 @@
+import logging
+import select
+import sys
+import termios
+import tty
+
+import pimm
+
+
+class KeyboardControl(pimm.ControlSystem):
+    def __init__(self):
+        self.keyboard_inputs = pimm.ControlSystemEmitter(self)
+
+    def run(self, should_stop: pimm.SignalReceiver, clock: pimm.Clock):
+        # Check if stdin is a TTY before attempting terminal operations
+        if not sys.stdin.isatty():
+            print('WARNING: KeyboardControl cannot read input - stdin is not a terminal', file=sys.stderr)
+            logging.warning('KeyboardControl cannot read input - stdin is not a terminal')
+            return
+
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        tty.setcbreak(fd)
+
+        try:
+            while not should_stop.value:
+                r, _, _ = select.select([sys.stdin], [], [], 0.0)
+                if r:
+                    self.keyboard_inputs.emit(sys.stdin.read(1))
+                yield pimm.Sleep(0.01)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
--- positronic/inference.py
+++ positronic/inference.py
@@ -1,9 +1,5 @@
 import logging
-import select
-import sys
-import termios
 import time
-import tty
 from collections.abc import Callable, Iterator, Mapping, Sequence
 from contextlib import nullcontext
 from dataclasses import dataclass
@@ -28,6 +24,7 @@ from positronic.dataset.ds_writer_agent import DsWriterCommand, Serializers, Tim
 from positronic.dataset.local_dataset import LocalDatasetWriter
 from positronic.drivers import roboarm
 from positronic.gui.dpg import DearpyguiUi
+from positronic.gui.keyboard import KeyboardControl
 from positronic.policy.action import ActionDecoder
 from positronic.policy.observation import ObservationEncoder
 from positronic.simulator.mujoco.observers import BodyDistance, StackingSuccess
@@ -169,31 +166,6 @@ class Inference(pimm.ControlSystem):
                 continue
             finally:
                 yield pimm.Sleep(rate_limiter.wait_time())
-
-
-class KeyboardControl(pimm.ControlSystem):
-    def __init__(self):
-        self.keyboard_inputs = pimm.ControlSystemEmitter(self)
-
-    def run(self, should_stop: pimm.SignalReceiver, clock: pimm.Clock):
-        # Check if stdin is a TTY before attempting terminal operations
-        if not sys.stdin.isatty():
-            print('WARNING: KeyboardControl cannot read input - stdin is not a terminal', file=sys.stderr)
-            logging.warning('KeyboardControl cannot read input - stdin is not a terminal')
-            return
-
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        tty.setcbreak(fd)
-
-        try:
-            while not should_stop.value:
-                r, _, _ = select.select([sys.stdin], [], [], 0.0)
-                if r:
-                    self.keyboard_inputs.emit(sys.stdin.read(1))
-                yield pimm.Sleep(0.01)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
 
 class KeyboardHanlder:
